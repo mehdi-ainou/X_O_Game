c13bfe433b79eab86288bff63ec46f0f5444861d
mehdi <elmahdiainou496@gmail.com>
2025-02-03 21:37:14 +0100

    refactor: (refactoring logic code)

--- logic.js
+++ logic.js
@@ -1,5 +1,7 @@
 let title = document.querySelector(".title");
+let squareContainer = document.querySelectorAll(".square")
 let turn = "X";
+arraySquare = [];
 
 
 function game(id) {
@@ -18,65 +20,74 @@ function game(id) {
 
     checkedWinner();
   }
-}
+};
 
-function checkedWinner() {
-    const arraySquare = document.querySelectorAll(".square");
-    
-  let x = 0;
-  let o = 0;
-  let xItem = 0;
-  let oItem = 0;
-
-  arraySquare.forEach((ele)=>{
-    const value = parseInt(ele.dataset.value);
-    
-    if(ele.innerHTML != ""){
-        if(ele.innerHTML == "X" ){
-            x += value
-            xItem += 1;
-        }else{
-            o += value;
-            oItem += 1;
-        }
+function checkedWinner(){
+    for(let i = 1 ; i < 10; i++){
+      arraySquare[i] = document.getElementById(`item${i}`).innerHTML;      
     }
-  });
+    if(arraySquare[1] == arraySquare[2] && arraySquare[2] == arraySquare[3] && arraySquare[3] != ""){
+      Winner(1,2,3);
+    }else if(arraySquare[4] == arraySquare[5] && arraySquare[5] == arraySquare[6] && arraySquare[6] != ""){
+      Winner(4,5,6);
+    }else if(arraySquare[7] == arraySquare[8] && arraySquare[8] == arraySquare[9] && arraySquare[9] != ""){
+      Winner(7,8,9);
+    }else if(arraySquare[1] == arraySquare[4] && arraySquare[4] == arraySquare[7] && arraySquare[7] != ""){
+      Winner(1,4,7);
+    }else if(arraySquare[2] == arraySquare[5] && arraySquare[5] == arraySquare[8] && arraySquare[8] != ""){
+      Winner(2,5,8);
+    }else if(arraySquare[3] == arraySquare[6] && arraySquare[6] == arraySquare[9] && arraySquare[6] != ""){
+      Winner(3,6,9);
+    }else if(arraySquare[1] == arraySquare[5] && arraySquare[5] == arraySquare[9] && arraySquare[9] != ""){
+      Winner(1,5,9);
+    }else if(arraySquare[3] == arraySquare[5] && arraySquare[5] == arraySquare[7] && arraySquare[7] != ""){
+      Winner(3,5,7);
+    }else if(arraySquare[1] != "" &&
+            arraySquare[2] != "" && 
+            arraySquare[3] != "" &&
+            arraySquare[4] != "" &&
+            arraySquare[5] != "" &&
+            arraySquare[6] != "" &&
+            arraySquare[7] != "" &&
+            arraySquare[8] != "" &&
+            arraySquare[9] != "" ){
+              title.innerHTML = "Draw";
 
-  if(x == 15 && xItem == 3 ){
-    Winner("X")
-    x = 0;
-    xItem = 0;
-   
-
-  }else if( o == 15 && oItem == 3 ){
-    Winner("O")
-    o = 0;
-    oItem = 0;
-
-  }
-
-  function Winner(w){
-    title.innerHTML = `${w} Winner`;
-
-    arraySquare.forEach((ele)=>{
-       
-        if(ele.innerHTML == w){
+              setInterval(() => {
+                title.innerHTML += "."
+              }, 1000);
             
-          ele.style.backgroundColor = "#000";
-        }
-      });
-
-      arraySquare.forEach(ele=>ele.disabled = true);
-
-    setInterval(() => {
-        title.innerHTML += ".";
-      }, 1000);
-    
-      setTimeout(() => {
-        location.reload();
-      }, 4000);
-  }
-
+              setTimeout(() => {
+                location.reload();
+              }, 4000);
+      
+    }
 }
+
+function Winner(num1, num2, num3){
+  title.innerHTML = `${arraySquare[num1]} : Winner`;
+  document.getElementById(`item${num1}`).style.backgroundColor = "#000";
+  document.getElementById(`item${num2}`).style.backgroundColor = "#000";
+  document.getElementById(`item${num3}`).style.backgroundColor = "#000";
+
+  setInterval(() => {
+    title.innerHTML += "."
+  }, 1000);
+
+  setTimeout(() => {
+    location.reload();
+  }, 4000);
+
+  squareContainer.forEach(ele=>ele.disabled = true);
+}
+
+
+
+
+
+
+
+
+
 
 
